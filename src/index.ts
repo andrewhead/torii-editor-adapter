c9232b5bc78cab77c12431d7090250705e404ccc
Andrew Head <head.andrewm@gmail.com>
2019-08-07 01:43:06 -0700

    Bugfix: Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import { SantokuConnector } from "./connector";
 import { EditorAdapter } from "./editor-adapter";
 import { Message } from "./message";
+import { SantokuAdapter } from "./santoku-adapter";
+import { VsCodeWebviewEditorConnector } from "./vscode-webview-editor-connector";
+import { WebSocketsEditorConnector } from "./websockets-editor-connector";
 
-export { SantokuConnector, EditorAdapter, Message };
+export {
+  EditorAdapter,
+  SantokuAdapter,
+  VsCodeWebviewEditorConnector,
+  WebSocketsEditorConnector,
+  SantokuConnector,
+  Message
+};
